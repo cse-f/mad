647bc33acfea7797a560c3c9d57eddaa612d23b9
cse-f <verify.moneymind@gmail.com>
2024-11-15 17:24:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 ANDROID STUDIO
-1.HELLO World---->       curl https://cse-f.github.io/mad/hello_world.js
+1.HELLO World---->            curl https://cse-f.github.io/mad/hello_world.js
 2.activiyLifeCycle---->       curl https://cse-f.github.io/mad/activity.js
-3.DiceRoll---->       curl https://cse-f.github.io/mad/dice_roll.js
-4.IncreamentCounter---->       curl https://cse-f.github.io/mad/increament_counter.js
-5.tip_calculator---->       curl https://cse-f.github.io/mad/tip_calci.js
-6.Fragments---->       curl https://cse-f.github.io/mad/fragments.js
-7.explicit_intent---->       curl https://cse-f.github.io/mad/Explicit_intent.js
-8.affirmation---->       curl https://cse-f.github.io/mad/affirmation.js
+3.DiceRoll---->               curl https://cse-f.github.io/mad/dice_roll.js
+4.IncreamentCounter---->      curl https://cse-f.github.io/mad/increament_counter.js
+5.tip_calculator---->         curl https://cse-f.github.io/mad/tip_calci.js
+6.Fragments---->              curl https://cse-f.github.io/mad/fragments.js
+7.explicit_intent---->        curl https://cse-f.github.io/mad/Explicit_intent.js
+8.affirmation---->            curl https://cse-f.github.io/mad/affirmation.js
 
 
 
