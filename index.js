aa968a79441dd689fbf0a26a12144de6dd9034b5
cse-f <verify.moneymind@gmail.com>
2024-11-15 14:43:48 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+
+ANDROID STUDIO
+1.HELLO World---->       curl https://cse-f.github.io/mad/hello_world.js
+2.activiyLifeCycle---->       curl https://cse-f.github.io/mad/activity.js
+3.DiceRoll---->       curl https://cse-f.github.io/mad/dice_roll.js
+4.IncreamentCounter---->       curl https://cse-f.github.io/mad/increament_counter.js
+5.tip_calculator---->       curl https://cse-f.github.io/mad/tip_calci.js
