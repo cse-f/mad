ee1e91e034bc49008d22a1d277c1c0e5055015c7
cse-f <verify.moneymind@gmail.com>
2024-11-16 00:05:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-
 ANDROID STUDIO
-1.HELLO World---->            curl https://cse-f.github.io/mad/hello_world.js
-2.activiyLifeCycle---->       curl https://cse-f.github.io/mad/activity.js
-3.DiceRoll---->               curl https://cse-f.github.io/mad/dice_roll.js
-4.IncreamentCounter---->      curl https://cse-f.github.io/mad/increament_counter.js
-5.tip_calculator---->         curl https://cse-f.github.io/mad/tip_calci.js
-6.Fragments---->              curl https://cse-f.github.io/mad/fragments.js
-7.explicit_intent---->        curl https://cse-f.github.io/mad/Explicit_intent.js
-8.affirmation---->            curl https://cse-f.github.io/mad/affirmation.js
-9.RoomDataBase ----->         curl https://cse-f.github.io/mad/RoomDataBase.js
+1.HELLO World---------->       curl https://cse-f.github.io/mad/hello_world.js
+2.activiyLifeCycle----->       curl https://cse-f.github.io/mad/activity.js
+3.DiceRoll------------->       curl https://cse-f.github.io/mad/dice_roll.js
+4.IncreamentCounter---->       curl https://cse-f.github.io/mad/increament_counter.js
+5.tip_calculator------->       curl https://cse-f.github.io/mad/tip_calci.js
+6.Fragments------------>       curl https://cse-f.github.io/mad/fragments.js
+7.explicit_intent------>       curl https://cse-f.github.io/mad/Explicit_intent.js
+8.affirmation---------->       curl https://cse-f.github.io/mad/affirmation.js
+9.RoomDataBase -------->       curl https://cse-f.github.io/mad/RoomDataBase.js
 
+KOTLIN
+1.GuessingNumber------->       curl https://cse-f.github.io/mad/GuessNumber.js
+2.PrinSquareOrNull----->       curl https://cse-f.github.io/mad/SquareOrNull.js
+3.GreetingMsg---------->       curl https://cse-f.github.io/mad/Greeting.js
+4.Dwellings(Hierarchy)->
   
 
 
